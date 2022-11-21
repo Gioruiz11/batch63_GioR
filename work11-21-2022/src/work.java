a2a70f0511a2b2d666d953411543166baaea9704
gioruiz11 <gioruiz11@hotmail.com>
2022-11-21 15:57:43 -0500

    d

---
+++ work11-21-2022/src/work.java
@@ -0,0 +1,50 @@
+
+public class work {
+
+	public int biggerNumber(int a, int b) {
+		if(a > b) {
+			return a;
+		}
+		return b;
+	}
+	
+	public int[] swapNum(int a, int b) {
+		int temp = a;
+		a = b;
+		b = temp;
+		int[] arr = {a,b};
+		return arr;
+	}
+	
+	public String evenOrOdd(int num) {
+		if(num % 2 == 0) {
+			return "even";
+		}
+		return "odd";
+	}
+	
+	public int biggerThreeNumbers(int a, int b, int c) {
+		if(a > b && a > c) {
+			return a;
+		}
+		if(b > a && b > c) {
+			return b;
+		}
+		return c;
+	}
+	
+	public String vowelOrConsonant(char c) {
+		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+			return "vowel";
+		}
+		return "consonant";
+	}
+	public static void main(String[] args) {
+		work w = new work();
+		System.out.println(w.biggerNumber(300, 203));
+		System.out.println(w.evenOrOdd(23));
+		System.out.println(w.biggerThreeNumbers(200, 250, 300));
+		System.out.println(w.vowelOrConsonant('r'));
+	}
+
+}
